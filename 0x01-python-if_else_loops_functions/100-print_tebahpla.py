e2231ecba07895583ffb2eec624697a7518b03e5
smilealien05 <ismailstarallah0@gmail.com>
2024-03-12 04:58:20 -0700

    modify

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
     if i % 2 == 1:
-        print(chr(i), end='')
+        print("{}".format(chr(i)), end='')
     else:
-        print(chr(i + 32), end='')
+        print("{}".format(chr(i + 32)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for i in range(97, 123):
     if i == 101 or i == 113:
         continue
-    print(f"{chr(i)}", end=' ')
+    print(f"{}",.format(chr(i)), end=' ')
