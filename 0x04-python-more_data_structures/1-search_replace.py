7eaf946ceaceeac9f1410e4b132dcf2782c37719
smilealien05 <ismailstarallah0@gmail.com>
2024-03-20 14:37:53 -0700

    add

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for r in new_list:
-        if r == saerch:
-            r = replace
+    for r in range(len(new_list)):
+        if new_list[r] == search:
+            new_list[r] = replace
     return new_list
