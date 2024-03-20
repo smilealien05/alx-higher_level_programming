3393477cf377c79056120a952ed2be84ac71b6e5
smilealien05 <ismailstarallah0@gmail.com>
2024-03-20 14:31:41 -0700

    first commit in the day with tasks of 0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for r in new_list:
+        if r == saerch:
+            r = replace
+    return new_list
