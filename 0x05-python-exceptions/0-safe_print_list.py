2aff2554153510aba14d03f41cc57a484feee56d
smilealien05 <ismailstarallah0@gmail.com>
2024-04-03 16:24:17 -0700

    change

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
-            num += 1
+sh: 1: q: not found
+            count += 1
         except IndexError:
             break
-    print("")
-    return
+        except:
+            pass
+    print()
+    return count
