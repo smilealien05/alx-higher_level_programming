43405c34d5843b0602e028f7536f138849b2c67d
smilealien05 <ismailstarallah0@gmail.com>
2024-05-17 03:36:30 -0700

    python-inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,9 @@ contains a class MyInt
 
 class MyInt(int):
     """MyInt is a rebel of int."""
-    def __new__(cls, *args, **kwargs):
-        """create a new instance of the class"""
-        return super(MyInt, cls).__new(cls, *args, **kwargs)
+    #def __new__(cls, *args, **kwargs):
+    #    """create a new instance of the class"""
+    #    return super(MyInt, cls).__new(cls, *args, **kwargs)
 
     def __eq__(self, other):
         """Reverse != to =="""
